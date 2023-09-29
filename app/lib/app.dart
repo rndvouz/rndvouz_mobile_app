74159e90121c8a723ec1c0006e24e7d0aeb1035f
Tracy Bui <buit@hawaii.edu>
2023-09-28 19:14:31 -1000

    created bottom navigation bar widget for the homescreen

---
+++ app/lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:app/homescreen.dart';
+import 'package:app/login_page.dart';
+import 'package:flutter/material.dart';
+
+class RndvouzApp extends StatelessWidget {
+  const RndvouzApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rndvouz App',
+      initialRoute: '/login',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/login': (BuildContext context) => LoginPage(),
+        '/homescreen': (BuildContext context) => const Homescreen(),
+      },
+    );
+
+    // Build Rndouz App Theme
+
+    // Build Rndvouz Text Theme
+  }
+}
