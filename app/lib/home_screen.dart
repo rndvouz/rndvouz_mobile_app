162901b8d71e1eaa1a8aef0f950a1989c3f7e3f5
Tracy Bui <buit@hawaii.edu>
2023-09-28 22:09:31 -1000

    added minor styling changes

--- app/lib/home_screen.dart
+++ app/lib/home_screen.dart
@@ -9,23 +9,40 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   int _selectedIndex = 0;
+  static const TextStyle tempTextStyle =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
 
   static const List<Widget> _homescreenPages = <Widget>[
-    Text('Home Button: Browse/Swipe mode to be implemented!'
-        // Add path to browse/swipe mode
-        ),
-    Text('Search Button: Search page to be implemented!'
-        // Add path to search
-        ),
-    Text('Upload Item Button: Upload item page to be implemented!'
-        // Add path to browse/swipe mode
-        ),
-    Text('Notification Button: Notification page to be implemented!'
-        // Add path to browse/swipe mode
-        ),
-    Text('Profile Button: Profile page to be implemented!'
-        // Add path to browse/swipe mode
-        ),
+    Text(
+      'Home Button: Browse/Swipe mode to be implemented!',
+      style: tempTextStyle,
+      textAlign: TextAlign.center,
+      // Add path to browse/swipe mode
+    ),
+    Text(
+      'Search Button: Search page to be implemented!',
+      style: tempTextStyle,
+      textAlign: TextAlign.center,
+      // Add path to search
+    ),
+    Text(
+      'Upload Item Button: Upload item page to be implemented!',
+      style: tempTextStyle,
+      textAlign: TextAlign.center,
+      // Add path to browse/swipe mode
+    ),
+    Text(
+      'Notification Button: Notification page to be implemented!',
+      style: tempTextStyle,
+      textAlign: TextAlign.center,
+      // Add path to browse/swipe mode
+    ),
+    Text(
+      'Profile Button: Profile page to be implemented!',
+      style: tempTextStyle,
+      textAlign: TextAlign.center,
+      // Add path to browse/swipe mode
+    ),
   ];
 
   void _onItemTapped(int index) {
@@ -43,41 +60,38 @@ class _HomescreenState extends State<Homescreen> {
       body: Center(
         child: _homescreenPages.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-            canvasColor: Colors.lightGreen,
-            primaryColor: Colors.black,
-            textTheme: Theme.of(context)
-                .textTheme
-                .copyWith(bodySmall: const TextStyle(color: Colors.grey))),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_box),
-              label: 'Upload Item',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications),
-              label: 'Notification',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          showSelectedLabels: false, 
-          showUnselectedLabels: false,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        // Fixed so that it does not expand and shift other icons when tapped
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.lightGreen,
+        selectedItemColor: Colors.brown,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box),
+            label: 'Upload Item',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
       ),
     );
   }
