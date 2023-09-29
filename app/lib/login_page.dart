5c905043c5110f31cff5cc42557fa1a04f3b4095
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-09-28 14:43:31 -1000

    Design Login Page UI Mockup

---
+++ app/lib/login_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(bottom: 50.0), 
+              child: Text(
+                'RNDVOUZ',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontFamily: 'RobotoMono',
+                  shadows: [
+                    Shadow(
+                      color: Colors.grey,
+                      offset: Offset(5, 5),
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 5,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Username',
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              elevation: 5,
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Handle login action here
+              },
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    // Handle "Forgot Password" action here
+                  },
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account?"
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Handle "Sign Up" action here
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
