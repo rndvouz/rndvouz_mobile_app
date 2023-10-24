632e9a210a1eb2108f1780b312cb9182542f3e6f
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-10-23 22:27:57 -1000

    add upload image component and add profile image (#53)

--- app/lib/setup_process/setup_profile.dart
+++ app/lib/setup_process/setup_profile.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:app/setup_process/setup_style.dart';
+import 'package:app/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import '../data_model/user_db.dart';
 
 import 'setup_top_bar.dart';
@@ -19,6 +23,7 @@ class SetupProfilePageState extends State<SetupProfilePage> {
   late TextEditingController usernameController;
   late User user;
   TextEditingController biographyController = TextEditingController();
+  Uint8List? selectedImage;
 
   SetupProfilePageState();
 
@@ -38,8 +43,8 @@ class SetupProfilePageState extends State<SetupProfilePage> {
         child: Column(
           children: [
             SetupTopBar(
-                state:
-                    widget.newUser.isBusiness ? 'profileBusiness' : 'profile'),
+              state: widget.newUser.isBusiness ? 'profileBusiness' : 'profile',
+            ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -48,7 +53,13 @@ class SetupProfilePageState extends State<SetupProfilePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       const SizedBox(height: 20), // Add spacing
-                      ProfilePictureUploadButton(onPressed: () {}),
+                      ImageSelectionButton(
+                        onImageSelected: (image) {
+                          setState(() {
+                            selectedImage = image;
+                          });
+                        },
+                      ),
                       const SizedBox(height: 20),
                       _buildTextField("Display Name", displayNameController),
                       _buildTextField("Username", usernameController),
@@ -58,19 +69,11 @@ class SetupProfilePageState extends State<SetupProfilePage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          // ElevatedButton(
-                          //   onPressed: () {
-                          //     Navigator.pop(context);
-                          //   },
-                          //   style: ElevatedButton.styleFrom(
-                          //     padding: const EdgeInsets.symmetric(vertical: 0),
-                          //   ),
-                          //   child: const Text('Back'),
-                          // ),
+
                           ElevatedButton(
                             onPressed: () {
-                              final updatedDisplayName =
-                                  displayNameController.text;
+                              // Handle 'Next' button action
+                              final updatedDisplayName = displayNameController.text;
                               final updatedBiography = biographyController.text;
                               final updatedUsername = usernameController.text;
                               try {
@@ -84,14 +87,14 @@ class SetupProfilePageState extends State<SetupProfilePage> {
                                     context,
                                     MaterialPageRoute(
                                         builder: (context) =>
-                                            widget.newUser.isBusiness
-                                                ? SetupStyle(
-                                                    newUser: widget.newUser)
-                                                : IndividualSetupSwipe(
-                                                    newUser: widget.newUser)));
+                                        widget.newUser.isBusiness
+                                            ? SetupStyle(
+                                            newUser: widget.newUser)
+                                            : IndividualSetupSwipe(
+                                            newUser: widget.newUser)));
                               } catch (e) {
                                 final exceptionMessage =
-                                    e.toString().replaceAll("Exception:", "");
+                                e.toString().replaceAll("Exception:", "");
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(
                                     content: Text(exceptionMessage),
@@ -119,9 +122,9 @@ class SetupProfilePageState extends State<SetupProfilePage> {
 
   Widget _buildTextField(String label, TextEditingController controller,
       {bool isObscure = false,
-      double width = 500,
-      double height = 60,
-      int lines = 1}) {
+        double width = 500,
+        double height = 60,
+        int lines = 1}) {
     return Container(
       width: width, // Set the width
       height: height, // Set the height
@@ -141,24 +144,50 @@ class SetupProfilePageState extends State<SetupProfilePage> {
   }
 }
 
-class ProfilePictureUploadButton extends StatelessWidget {
-  final Function() onPressed;
+class ImageSelectionButton extends StatefulWidget {
+  final Function(Uint8List?) onImageSelected;
 
-  const ProfilePictureUploadButton({Key? key, required this.onPressed})
+  const ImageSelectionButton({Key? key, required this.onImageSelected})
       : super(key: key);
+
+  @override
+  _ImageSelectionButtonState createState() => _ImageSelectionButtonState();
+}
+
+class _ImageSelectionButtonState extends State<ImageSelectionButton> {
+  Uint8List? _image;
+
+  void selectImage(ImageSource source) async {
+    Uint8List? img = await pickAndCropImage(source);
+    if (img != null) {
+      setState(() {
+        _image = img;
+      });
+      widget.onImageSelected(_image);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onPressed,
+      onTap: () {
+        ImageOptionsUtil.showImageSourceOptions(context, (imageSource) => selectImage(imageSource));
+      },
       child: Container(
-        width: 150,
-        height: 150,
+        width: 250,
+        height: 250,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           color: Colors.grey[300],
         ),
-        child: Center(
+        child: _image != null
+            ? ClipOval(
+          child: Image.memory(
+            _image!,
+            fit: BoxFit.cover,
+          ),
+        )
+            : Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -167,7 +196,7 @@ class ProfilePictureUploadButton extends StatelessWidget {
                 size: 40,
                 color: Colors.grey[600],
               ),
-              const SizedBox(height: 8), // Add spacing between Icon and Text
+              const SizedBox(height: 8),
               Text(
                 'Add Photo',
                 style: TextStyle(
