04dfe5342c5d1c3d42dd2b7645662219332a04cf
tiffanywilliams <60216343+tiffanywilliams@users.noreply.github.com>
2023-11-14 01:56:34 -1000

    upload item wip

--- lib/features/home/presentation/home_upload_item.dart
+++ lib/features/home/presentation/home_upload_item.dart
@@ -1,20 +1,145 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-// Change to Stateful when you can tap on image to view more information about it
-class HomeUploadItem extends StatelessWidget {
+import 'package:rndvouz/features/home/presentation/upload_body.dart';
+import 'package:rndvouz/features/setup_process/presentation/setup_style.dart';
+import 'package:rndvouz/features/common/presentation/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import '../../user/domain/user_db.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeUploadItem extends ConsumerWidget {
   const HomeUploadItem({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final displayNameController = TextEditingController(text: 'hi');
+    final usernameController = TextEditingController(text: 'cry');
+    final biographyController = TextEditingController();
+    Uint8List? selectedImage;
     return Scaffold(
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("UPLOAD ITEM PAGE TO BE IMPLEMENTED!"),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const SizedBox(height: 20), // Add spacing
+                      ImageSelectionButton(
+                        onImageSelected: (image) {
+                          selectedImage = image;
+                        },
+                      ),
+                      UploadBody(),
+                      const SizedBox(height: 20),
+                      _buildTextField("Display Name", displayNameController),
+                      _buildTextField("Username", usernameController),
+                      _buildTextField('Biography', biographyController,
+                          width: 500, height: 120, lines: 3),
+                      const SizedBox(height: 40),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller,
+      {bool isObscure = false,
+      double width = 500,
+      double height = 60,
+      int lines = 1}) {
+    return Container(
+      width: width, // Set the width
+      height: height, // Set the height
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: TextField(
+        controller: controller,
+        obscureText: isObscure,
+        maxLines: lines,
+        decoration: InputDecoration(
+          labelText: label,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ImageSelectionButton extends StatefulWidget {
+  final Function(Uint8List?) onImageSelected;
+
+  const ImageSelectionButton({Key? key, required this.onImageSelected})
+      : super(key: key);
+
+  @override
+  _ImageSelectionButtonState createState() => _ImageSelectionButtonState();
+}
+
+class _ImageSelectionButtonState extends State<ImageSelectionButton> {
+  Uint8List? _image;
+
+  void selectImage(ImageSource source) async {
+    Uint8List? img = await pickAndCropImage(source);
+    if (img != null) {
+      setState(() {
+        _image = img;
+      });
+      widget.onImageSelected(_image);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        ImageOptionsUtil.showImageSourceOptions(
+            context, (imageSource) => selectImage(imageSource));
+      },
+      child: Container(
+        width: 250,
+        height: 250,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.grey[300],
+        ),
+        child: _image != null
+            ? ClipOval(
+                child: Image.memory(
+                  _image!,
+                  fit: BoxFit.cover,
+                ),
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add_a_photo_outlined,
+                      size: 40,
+                      color: Colors.grey[600],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      'Add Photo',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
   }
