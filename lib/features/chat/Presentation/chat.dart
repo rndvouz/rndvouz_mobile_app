8d4d4511156b1f38ef6cb9f2789c063546896064
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-11-14 14:06:39 -1000

    add mock up for message page

---
+++ lib/features/chat/Presentation/chat.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:rndvouz/features/Chat/domain/chat_message.dart';
+
+
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+  ChatMessage(messageContent: "Hey Kriss, I am doing fine dude. wbu?", messageType: "sender"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+  ChatMessage(messageContent: "Is there any thing wrong?", messageType: "sender"),
+];
+
+class SingleChat extends StatelessWidget {
+  const SingleChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back, color: Colors.black,),
+                ),
+                SizedBox(width: 12,),
+                CircleAvatar(
+                  maxRadius: 20,
+                ),
+                SizedBox(width: 12,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Kriss Benwat", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
+                      SizedBox(height: 6,),
+                      Text("Online", style: TextStyle(color: Colors.grey.shade600, fontSize: 13),),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 12,),  // Add some padding
+                Icon(Icons.settings, color: Colors.black54,),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children:<Widget>[
+          ListView.builder(
+            itemCount: messages.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index){
+              return Container(
+                padding: EdgeInsets.only(left: 16,right: 16,top: 10),
+                child: Align(
+                  alignment: (messages[index].messageType == "receiver"? Alignment.topLeft: Alignment.topRight),
+                  child:  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (messages[index].messageType == "receiver"? Colors.grey.shade200:Colors.blue),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(messages[index].messageContent, style: TextStyle(fontSize: 15),),
+                  ),
+                ),
+              );
+            },
+          ),
+        Column(
+        mainAxisAlignment: MainAxisAlignment.end , // Align the children at the bottom
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 3),
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    // Handle onTap
+                  },
+                  child: Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Icon(Icons.add, color: Colors.white, size: 20,),
+                  ),
+                ),
+                SizedBox(width: 15,),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Write message...",
+                      hintStyle: TextStyle(color: Colors.black54),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  child: Icon(Icons.send, color: Colors.white, size: 18,),
+                  backgroundColor: Colors.blue,
+                  elevation: 0,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ],
+    )
+    );
+  }
+}
